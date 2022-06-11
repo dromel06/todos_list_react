089cb5de1304ead39db94ade9e8c24a086f8599d
Dromel <d.romeldiaz@gmail.com>
2022-06-10 19:14:56 -0500

    qwerty

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,15 +3,42 @@ import { AppUI } from "./AppUI";
 import "./App.css";
 
 
-const defaultTodos = [
-	{ text: 'Cortar Cebollas', completed: false },
-	{ text: 'Hacer una tarea', completed: true },
-	{ text: 'Hacer otra tarea', completed: false },
-	{ text: 'tarea', completed: true },
-];
+// const defaultTodos = [
+	// { text: 'Cortar Cebollas', completed: false },
+	// { text: 'Hacer una tarea', completed: true },
+	// { text: 'Hacer otra tarea', completed: false },
+	// { text: 'tarea', completed: true },
+// ];
+
+function useLocalStorage(itemName, initialValue) {
+
+	const localStorageItem = localStorage.getItem(itemName);
+	let parsedItem;
+
+	if (!localStorageItem) {
+		localStorage.setItem(itemName, JSON.stringify(initialValue));
+		parsedItem = [];
+	}
+
+	else {
+		parsedItem = JSON.parse(localStorageItem);
+	}
+
+	const [item, setItem] = React.useState(parsedItem);
+
+	const saveItem = (newItem) => {
+		setItem(newItem);
+		localStorage.setItem("TODOS_V1", JSON.stringify(newItem));
+	}
+
+	return [item, saveItem];
+
+}
 
 function App() {
-	const [todos, setTodos] = React.useState(defaultTodos);
+	const [todos, saveTodos] = useLocalStorage("TODOS_V1", []);
+
+
 	const [searchValue, setSearchValue] = React.useState('');
 
 	const completedTodos = todos.filter(todo => todo.completed).length;
@@ -29,19 +56,26 @@ function App() {
 		})
 	}
 
+	
+
+
 	const completeTodo = (text) => {
 		const todoIndex = todos.findIndex(todo => todo.text === text);
 		const newTodos = [...todos];
 		newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
-		setTodos(newTodos);
+		saveTodos(newTodos);
 	}
 
 	const deleteTodo = (text) => {
 		const todoIndex = todos.findIndex(todo => todo.text === text);
 		const newTodos = [...todos];
 		newTodos.splice(todoIndex, 1)
-		setTodos(newTodos);
+		saveTodos(newTodos);
 	}
+
+	React.useEffect(() => {
+		console.log("useEffect");
+	});
 
 	return (
 		<AppUI 
