c47812e679e0ea1166a53a1819a7e2d756ececcd
Dromel <d.romeldiaz@gmail.com>
2022-06-13 16:11:35 -0500

    Modal

--- src/components/TodoSearch/index.jsx
+++ src/components/TodoSearch/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './TodoSearch.css'
+import { TodoContext } from '../TodoContext';
 
-function TodoSearch({searchValue, setSearchValue}) {
-    // const [searchValue, setSearchValue] = React.useState('');
+function TodoSearch() {
+    const {searchValue, setSearchValue} = React.useContext(TodoContext);
 
 
     const onSearchValueChange = (event) => {
