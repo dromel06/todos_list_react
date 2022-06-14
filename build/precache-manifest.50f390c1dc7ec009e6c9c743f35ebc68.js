d9e59f7229c8f9e7bbd76690bf46e762ed536b53
Dromel <d.romeldiaz@gmail.com>
2022-06-14 14:38:45 -0500

    deploy

---
+++ build/precache-manifest.50f390c1dc7ec009e6c9c743f35ebc68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3d99faa7bce40a5c87c",
+    "url": "/todos-list-react/static/css/main.36262289.chunk.css"
+  },
+  {
+    "revision": "b3d99faa7bce40a5c87c",
+    "url": "/todos-list-react/static/js/main.b3d99faa.chunk.js"
+  },
+  {
+    "revision": "db389241ecef3d330313",
+    "url": "/todos-list-react/static/js/1.db389241.chunk.js"
+  },
+  {
+    "revision": "b9fff6ee0465d04bb035",
+    "url": "/todos-list-react/static/js/runtime~main.b9fff6ee.js"
+  },
+  {
+    "revision": "fd9125261ccf6e92ee10dcddebda8642",
+    "url": "/todos-list-react/index.html"
+  }
+];
